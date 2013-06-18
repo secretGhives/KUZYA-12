b43f3f5d7cf8843ef0e4f6f7be219dfc2254ab3d
Hooker Juggernaut <secretgspot@gmail.com>
2013-06-18 04:11:23 -0600

    mm

--- js/script.js
+++ js/script.js
@@ -23,20 +23,28 @@ $(document).ready(function() {
 		show_more: 19
 	 });
 
+	jQuery("#traditional").EmbedPicasaGallery('secretGspot',{
+		albumid: "5858417312742104513",
+		size: 160, // thumb size (32,48,64,72,144,160))
+		loading_animation: "/img/loading.gif",
+		msg_more: 'more',
+		show_more: 19
+	 });
+
 	// TWEETER
 	////////////////////////
-	$("#latest-tweets").tweet({
-		join_text: " ",
-		username: "secretgspot",
-		avatar_size: 48,
-		count: 6,
-		// auto_join_text_default: "we said,",
-		// auto_join_text_ed: "we",
-		// auto_join_text_ing: "we were",
-		// auto_join_text_reply: "we replied",
-		// auto_join_text_url: "we were checking out",
-		loading_text: "loading tweets..."
-	});
+	// $("#latest-tweets").tweet({
+	// 	join_text: " ",
+	// 	username: "secretgspot",
+	// 	avatar_size: 48,
+	// 	count: 6,
+	// 	// auto_join_text_default: "we said,",
+	// 	// auto_join_text_ed: "we",
+	// 	// auto_join_text_ing: "we were",
+	// 	// auto_join_text_reply: "we replied",
+	// 	// auto_join_text_url: "we were checking out",
+	// 	loading_text: "loading tweets..."
+	// });
 
 
 });
